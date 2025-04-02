a1fc6ce3d20143a43d77d9658ff66516f9f4a9ec
SumantSingh08 <163447970+SumantSingh08@users.noreply.github.com>
2025-04-02 21:28:21 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { RouterProvider,  createHashRouter ,  } from 'react-router-dom'
 import App from './App.jsx'
 import Meal from './components/mealInfo.jsx'
 import Layout from './Layout.jsx'
 
-const route = createBrowserRouter(
+/* const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<App/>} />
       <Route path='recipe/:id' element={<Meal/>} />
    </Route>
   )
-)
+) */
+const route = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '', element: <App /> },
+      { path:'recipe/:id', element: <Meal /> }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={route}/>
 )
